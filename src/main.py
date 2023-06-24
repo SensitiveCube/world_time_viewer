f0c6a4f564cd389279640167237f90eb9f4a1eb1
SensitiveCube <39450043+SensitiveCube@users.noreply.github.com>
2023-06-24 16:11:10 +0900

    add feature file

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import sys
+import pytz
+from datetime import datetime
+from PySide6.QtCore import Qt, QTimer
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QVBoxLayout, QWidget
+
+class WorldTimeViewerApp(QMainWindow):
+    def __init__(self):
+        pass
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+import pytest
+from PySide6.QtCore import Qt, QDateTime
+from PySide6.QtTest import QTest
+from PySide6.QtWidgets import QApplication
+from src.main import WorldTimeViewerApp
